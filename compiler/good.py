a6a7c456470f12f169d1eef12372dd1f623169b5
dinggul <june.parkminjoon@gmail.com>
2017-03-15 21:39:31 +0900

    Add a wrapper program for given compiler
    Given language is difficult to use since a lack of syntatic
    sugars such as label, comment, and conversion char to int.
    good.py contains these functions, so it is a really good program.
    With this, I didn't need any debug process for implementing login.mini.

---
+++ compiler/good.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python2
+
+import os
+import sys
+import re
+
+label, code = {}, []
+
+def remove_comment(s):
+    return s.split('#', 1)[0]
+
+def trim(s):
+    from string import whitespace as ws
+
+    while len(s) > 0 and s[0] in ws:
+        s = s[1:]
+
+    while len(s) > 0 and s[-1] in ws:
+        s = s[:-1]
+
+    return s
+
+def parse_arguments(ins):
+    global label, code
+
+    def arg_iter(s):
+        while len(s) != 0:
+            if re.match(r"^'(.)'", s):
+                yield str(ord(re.match(r"^'(.)'", s).groups()[0]))
+                s = s[3:]
+            elif re.match(r'^0x([a-f0-9]+)', s):
+                yield str(int(re.match(r'^0x([a-f0-9]+)', s).groups()[0], 16) & 0xff)
+                if ',' in s:
+                    s = trim(s.split(',', 1)[1])
+                else:
+                    s = ''
+            elif re.match(r'^(r[0-9]+)', s):
+                yield re.match(r'^(r[0-9]+)', s).groups()[0]
+                if ',' in s:
+                    s = trim(s.split(',', 1)[1])
+                else:
+                    s = ''
+            elif re.match(r'^([0-9]+)', s):
+                yield re.match(r'^([0-9]+)', s).groups()[0]
+                if ',' in s:
+                    s = trim(s.split(',', 1)[1])
+                else:
+                    s = ''
+            elif re.match(r'^([a-zA-Z_][a-zA-Z_0-9]*)', s):
+                l = re.match(r'^([a-zA-Z_][a-zA-Z_0-9]*)', s).groups()[0]
+                assert l in label, 'No such label: [{}]'.format(l)
+
+                yield str(label[l])
+                if ',' in s:
+                    s = trim(s.split(',', 1)[1])
+                else:
+                    s = ''
+            else:
+                assert False, 'No such pattern: [{}]'.format(s)
+
+    res = ins.split(' ', 1)
+    if len(res) == 1:
+        return ins, []
+
+    ins_head, args = res
+    return ins_head, list(arg_iter(args))
+
+assert len(sys.argv) > 1, 'Give me more arguments'
+assert sys.argv[1].endswith('.mini'), 'Invalid extension'
+assert os.path.exists(sys.argv[1]), 'No such file'
+
+with open(sys.argv[1], 'rb') as f:
+    data = f.read().split('\n')
+    data = [trim(remove_comment(i)) for i in data]
+    data = [i for i in data if i != '']
+
+
+for ins in data:
+    if ins.endswith(':'):
+        l = trim(ins.split(':', 1)[0])
+        label[l] = len(code)
+    else:
+        code.append(ins)
+
+code = map(parse_arguments, code)
+
+dirname = os.path.dirname(sys.argv[1])
+with open(os.path.join(dirname, 'tmp.mini'), 'wb') as f:
+    f.write('\n'.join(trim('{} {}'.format(h, ', '.join(a))) for h, a in code))
+
+fn = sys.argv[1][:-5]
+print fn
+os.system('ocaml str.cma compiler.ml {}/tmp.mini {}.out'.format(dirname, fn))
